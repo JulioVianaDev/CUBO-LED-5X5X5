36471f72a6fbd7058add1360728e7c9040b5ae68
juliovianadev <juiovianadev@gmail.com>
2022-10-31 23:30:45 -0300

    Animation 4 finalizada

--- CUBO_LED_5X5X5.ino
+++ CUBO_LED_5X5X5.ino
@@ -8,7 +8,7 @@ int columnPin[25]={53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,3
 //coluna 15= 38    coluna 16= 37   coluna 17= 36   coluna 18= 35   coluna 19= 34
 //coluna 20= 33    coluna 21= 32   coluna 22= 31   coluna 23= 30   coluna 24= 29
 //int columnPinA2[25]={53,52,51,50,59,44,39,34,29,30,31,32,33,38,43,48,47,46,45,40,35,36,37,42,41};
-
+//  
 void setup() {
   //setando os valores das layer como saida
   for(int i=0;i<5;i++){
@@ -107,8 +107,45 @@ void Animation3(){
 }
 
 void Animation4(){
-    digitalWrite(layerPin[2],LOW);
-    digitalWrite(columnPin[12],HIGH);
+  //acender o miolo
+  digitalWrite(layerPin[2],LOW);
+  digitalWrite(columnPin[12],HIGH);
+  delay(300);
+  //acender os 3 do meio
+  int colunas3[9]={13,12,11,17,16,18,8,7,6};
+  for(int i=0;i<10;i++){
+    digitalWrite(columnPin[colunas3[i]],HIGH);   
+  }
+  digitalWrite(layerPin[1],LOW);
+  digitalWrite(layerPin[3],LOW);
+  //acender tudo
+  delay(300);
+  for(int i=0;i<25;i++){
+    digitalWrite(columnPin[i],HIGH);
+  }
+  digitalWrite(layerPin[0],LOW);
+  digitalWrite(layerPin[4],LOW);
+  delay(300);
+  //reverter a partir de agora
+  digitalWrite(layerPin[0],HIGH);
+  digitalWrite(layerPin[4],HIGH);
+  int colunas5Apagar[16]={0,1,2,3,4,9,14,19,24,23,22,21,20,15,10,5};
+  for(int i=0;i<16;i++){
+    digitalWrite(columnPin[colunas5Apagar[i]],LOW);
+  }
+  delay(300);
+  //reverter os 3
+  int colunas3Apagar[8]={6,7,8,13,18,17,16,11};
+  digitalWrite(layerPin[1],HIGH);
+  digitalWrite(layerPin[3],HIGH);
+  for(int i=0;i<8;i++){
+    digitalWrite(columnPin[colunas3Apagar[i]],LOW);
+  }
+  delay(300);
+  //apagar o que sobrou
+  digitalWrite(layerPin[2],HIGH);
+  digitalWrite(columnPin[7],LOW);
+  delay(300);
 }
 void ZIGZAG(int columnPinA3[]){
   for(int i=0;i<25;i++){
